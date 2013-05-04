00f41f7007ec30971745133c79ba5141a3cc5b4a
sphinxen <sphinxen83@gmail.com>
2013-05-04 12:47:24 +0200

    Removed rewrite base, changed message on missing config file

--- system/core/bootstrap.php
+++ system/core/bootstrap.php
@@ -17,7 +17,7 @@ if(file_exists(ROOT.'application/config.php'))
    require_once ROOT.'application/config.php';
 else
 //   require_once ROOT.'application/config.tpl.php';
-   die("Can't locate the 'config.php' file in the application folder");
+   die("Can't locate the 'config.php' file in the application folder. <br>Please generate \"application/config.php\" or copy \"application/config.tpl.php\" in your install directory and configure it to match your setup.");
 
 /**
  * 	Enable auto-load of class declarations.
