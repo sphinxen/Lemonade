a8c2440d595fa2d00f6adc517435773cf4ce257d
sphinxen <sphinxen83@gmail.com>
2013-06-25 12:07:16 +0200

    Default title

--- application/views/default/index.php
+++ application/views/default/index.php
@@ -26,7 +26,7 @@
 			<div class="logo clearfix">
 				<a class="" href="<?php echo BASE?>">
 				<span class="left">
-					<?php echo !empty($logo) ? '<img src="'.$logo.'" alt="Logo" />' : '';?>Lemonade
+					<?php echo !empty($logo) ? '<img src="'.$logo.'" alt="Logo" />' : '';?><?php echo $title;?>
 				</span>
 				</a>
 			</div>
