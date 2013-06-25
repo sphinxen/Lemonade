7b06f2a2ba6d8c9f4bde5481ce5205ab0b9f09b9
sphinxen <sphinxen83@gmail.com>
2013-06-25 21:08:00 +0200

    Cleanup in config file

--- application/config.tpl.php
+++ application/config.tpl.php
@@ -43,33 +43,25 @@ $cfg = array(
 */
 ,'default_page' => 'index'
 
+/**
+*	Set the session name
+*
+*/
 ,'session_name' => 'lemonade'
 
 
+/**
+*	Som default data that are used when generating view
+*
+*/
 ,'data' => array(
 
-	// Sets a text that will be shown at the top of the page next to the logo
-	'textlogo' => 'Lemonade'
-
-	// Sets a logo that will be shown at the top of the page next to the textlogo
-	,'logo' => BASE.'assets/images/logo.svg'
-
-
-	,'menu' => array(
-
-		// Defines the main-menu
-		'main' => array(
-					'id' => 'main-nav'
-					,'class' => NULL
-					,'items' => array(
-						 'Home' => array('path' => BASE.'index', 'id' => NULL, 'class' => NULL)
-						)
-				)
+	'menu' => array(
 
 		//	Defines the admin-menu only shown when a user with admin rights are logged in
-		,'admin' => array(
+		'admin' => array(
 					'id' => 'admin-nav'
-					,'class' => NULL
+					,'class' => 'menu'
 					,'items' => array(
 						'Manage Content' => array('path' => BASE.'content/', 'id' => NULL, 'class' => NULL)
 						)
@@ -77,32 +69,13 @@ $cfg = array(
 
 		,'user' => array(
 					'id' => 'user-nav'
-					,'class' => NULL
+					,'class' => 'menu'
 					,'items' => array(
 						'Logout' =>  array('path' => BASE.'user/logout', 'id' => NULL, 'class' => NULL)
 						)
 				)
 		)
 
-	// Defines slylesheets
-	,'stylesheets' => array(
-				BASE.'assets/css/stylesheet.css'
-				,BASE.'assets/js/markitup/skins/markitup/style.css'
-				,BASE.'assets/js/markitup/sets/default/style.css'
-				)
-
-
-	// Defines javascript files
-	,'javascripts' => array(
-				 BASE.'assets/js/jquery-1.7.1.js'
-				//,BASE.'assets/js/jquery.colorbox.js'
-				,BASE.'assets/js/markitup/jquery.markitup.js'
-				,BASE.'assets/js/markitup/sets/default/set.js'
-				,BASE.'assets/js/lemonade.js'
-				)
-
-	// Defines the title of the page shown by the browser
-	,'title' => 'Lemonade'
 	)
 
 );
